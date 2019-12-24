0c198ba2e904652a0ace16c8d4922e5633f77f98
priyanjitdey94 <priyanjit.dey@gmail.com>
2019-12-24 15:23:56 +0530

    created header visuals

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Avatar extends Component {
+  render () {
+    let { imageSrc, classNames, width, height } = this.props;
+    return (
+      <div  className={classNames}>
+        <img src={imageSrc} alt='avatar' className={'rounded-circle'} style={{ width, height }}/>
+      </div>
+    );
+  }
+}
+
+Avatar.defaultProps = {
+  classNames: '',
+  width: 40,
+  height: 40
+}
