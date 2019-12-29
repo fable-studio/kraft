52f716156efa79bb5bfb1179203288268bd596fd
siawo <md.owais1994@gmail.com>
2019-12-30 04:33:51 +0530

    Added ordering in card

--- src/components/InfographItem/card.js
+++ src/components/InfographItem/card.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './card.scss';
+const alphabetsCode = 65;
 
 const Card = (props) => {
-  const indexDiv = (<div className="col-2 index">
-      <h1>A</h1>
+  const character = props.isNumeric ? String(props.index + 1) : String.fromCharCode(alphabetsCode + (props.index % 26)),
+    indexDiv = (<div className="col-2 index">
+      <h1>{character}</h1>
     </div>),
-    textDiv = (<div className="col-10" >
+    textDiv = (<div className="col" >
       <div className="card-body">
         <h5 className="card-title">{props.title}</h5>
         <p className="card-text">{props.content}</p>
       </div>
     </div>),
     order = props.isLeftAligned ? [indexDiv, textDiv] : [textDiv, indexDiv];
-
-  return (<div className={'card card-view' + ` text-${props.textAlign}`} onClick={() => props.clicked(props.index)}>
+  return (<div className={'card card-view ' + (props.selected ? 'border-dark ' : '') + props.textAlign} onClick={() => props.clicked(props.index)}>
     <div className="row no-gutters">
       {
         order
--- src/components/InfographItem/tag-text.js
+++ src/components/InfographItem/tag-text.js
@@ -4,7 +4,7 @@ import BaseItemIcon from './base';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStream, faPlus, faTimes, faAlignLeft, faAlignRight, faAlignCenter } from '@fortawesome/free-solid-svg-icons';
 import Card from './card';
-import { Button, Input } from 'reactstrap';
+import { Button, ButtonGroup, Input } from 'reactstrap';
 
 class TagText extends Component {
  state = {
@@ -13,21 +13,21 @@ class TagText extends Component {
    cards: [{
      id: 'card-0',
      title: 'Lorem ipsum',
-     textAlign: 'left',
+     textAlign: 'text-left',
      cardAlign: 'left',
      content: 'Lorem ipsum dolor sit amet, eu sed liber audiam dolores, probo saepe verterem ius an. Quo lorem definitionem at. Et nulla malorum percipitur ius, possit latine splendide pro no'
    },
    {
      id: 'card-1',
      title: 'Lorem ipsum',
-     textAlign: 'left',
+     textAlign: 'text-left',
      cardAlign: 'left',
      content: 'Lorem ipsum dolor sit amet, eu sed liber audiam dolores, probo saepe verterem ius an. Quo lorem definitionem at. Et nulla malorum percipitur ius, possit latine splendide pro no'
    },
    {
      id: 'card-2',
      title: 'Lorem ipsum',
-     textAlign: 'left',
+     textAlign: 'text-left',
      cardAlign: 'left',
      content: 'Lorem ipsum dolor sit amet, eu sed liber audiam dolores, probo saepe verterem ius an. Quo lorem definitionem at. Et nulla malorum percipitur ius, possit latine splendide pro no'
    }]
@@ -39,7 +39,7 @@ class TagText extends Component {
      cards.push({
        id: `card-${cards.length}`,
        title: 'Lorem ipsum',
-       textAlign: 'left',
+       textAlign: 'text-left',
        cardAlign: 'left',
        content: 'Lorem ipsum dolor sit amet, eu sed liber audiam dolores, probo saepe verterem ius an. Quo lorem definitionem at. Et nulla malorum percipitur ius, possit latine splendide pro no'
      });
@@ -69,12 +69,12 @@ class TagText extends Component {
 
  }
 
- upadetTextAlignemntLeft = () => {
+ upadetTextAlignemnt = (alignment) => {
    if (this.state.selectedIndex === null) return;
    this.setState(state => {
      const cards = state.cards.slice(),
        card = Object.assign({}, cards[state.selectedIndex]);
-     card.textAlign = 'left';
+     card.textAlign = alignment;
      cards[state.selectedIndex] = card;
      return {
        ...state,
@@ -82,34 +82,6 @@ class TagText extends Component {
      };
    });
  }
-
- upadetTextAlignemntRight = () => {
-   if (this.state.selectedIndex === null) return;
-   this.setState(state => {
-     const cards = state.cards.slice(),
-       card = Object.assign({}, cards[state.selectedIndex]);
-     card.textAlign = 'right';
-     cards[state.selectedIndex] = card;
-     return {
-       ...state,
-       cards
-     };
-   });
- }
-
-upadetTextAlignemntCenter = () => {
-  if (this.state.selectedIndex === null) return;
-  this.setState(state => {
-    const cards = state.cards.slice(),
-      card = Object.assign({}, cards[state.selectedIndex]);
-    card.textAlign = 'center';
-    cards[state.selectedIndex] = card;
-    return {
-      ...state,
-      cards
-    };
-  });
-}
 
  updateTitle = (e) => {
    if (this.state.selectedIndex === null) return;
@@ -151,41 +123,44 @@ upadetTextAlignemntCenter = () => {
  }
 
  render () {
-   const { title, content } = this.state.selectedIndex === null ? {
-     title: '',
-     content: ''
-   } : this.state.cards[this.state.selectedIndex];
+   const { selectedIndex, cards } = this.state,
+     { title, content } = selectedIndex === null ? {
+       title: '',
+       content: ''
+     } : cards[selectedIndex],
+     { textAlign } = cards[selectedIndex];
    return (
      <>
        <Item.Infograph>
          {
-           this.state.cards.map(({ id, title, content, textAlign, cardAlign }, index) =>
-             <Card key={id} index={index} title={title} content={content} clicked={this.cardSelected} textAlign={textAlign} cardAlign={cardAlign} ></Card>)
+           cards.map(({ id, title, content, textAlign, cardAlign }, index) =>
+             <Card key={id} index={index} title={title} content={content} clicked={this.cardSelected} textAlign={textAlign} cardAlign={cardAlign} selected={index === selectedIndex} ></Card>)
          }
        </Item.Infograph>
        <Item.Editor>
          <div className='mx-3 my-3'>
            <div>
-             <Button className='mr-1' onClick={this.addCard}>
-               <FontAwesomeIcon icon={faPlus} />
-             </Button>
-             <Button className='mr-1' onClick={this.deleteCard}>
-               <FontAwesomeIcon icon={faTimes} />
-             </Button>
-           </div>
-           <div className='mt-2'>
-             <span className='font-weight-bold'>Text</span>
-           </div>
-           <div>
-             <Button className='mr-1' onClick={this.upadetTextAlignemntLeft}>
-               <FontAwesomeIcon icon={faAlignLeft} />
-             </Button>
-             <Button className='mr-1' onClick={this.upadetTextAlignemntCenter}>
-               <FontAwesomeIcon icon={faAlignCenter} />
-             </Button>
-             <Button className='mr-1' onClick={this.upadetTextAlignemntRight}>
-               <FontAwesomeIcon icon={faAlignRight} />
-             </Button>
+             <ButtonGroup size='sm'>
+               <Button active className='mr-1' onClick={this.addCard}>
+                 <FontAwesomeIcon icon={faPlus} />
+               </Button>
+               <Button active className='mr-1' onClick={this.deleteCard}>
+                 <FontAwesomeIcon icon={faTimes} />
+               </Button>
+             </ButtonGroup>
+           </div><div className='info-text-justify'>
+             <span className='mr-2'>Alignment:</span>
+             <ButtonGroup size='sm'>
+               <Button active={textAlign === 'text-left'} className='font-weight-bold text-alignment-btn' onClick={() => { this.upadetTextAlignemnt('text-left'); }}>
+                 <FontAwesomeIcon icon={faAlignLeft} />
+               </Button>
+               <Button active={textAlign === 'text-center'} className='font-weight-bold text-alignment-btn' onClick={() => { this.upadetTextAlignemnt('text-center'); }}>
+                 <FontAwesomeIcon icon={faAlignCenter} />
+               </Button>
+               <Button active={textAlign === 'text-right'} className='font-weight-bold text-alignment-btn' onClick={() => { this.upadetTextAlignemnt('text-right'); }}>
+                 <FontAwesomeIcon icon={faAlignRight} />
+               </Button>
+             </ButtonGroup>
            </div>
            <div className='mt-2'>
              <span className='font-weight-bold'>Title:</span>
